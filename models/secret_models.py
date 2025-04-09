0964adbc02c5863e59eb4a447f65c0981e27226a
nicola <belchuk2005@mail.ru>
2025-04-09 18:02:14 +0300

    Base secret db models

---
+++ models/secret_models.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, DateTime, Integer, String, Text
+from sqlalchemy.orm import Mapped
+
+from core.database import Base
+
+
+class Secret(Base):
+    """Secrets table model"""
+    __tablename__ = 'secrets'
+    secret_key: Mapped[str] = Column(String, primary_key=True, index=True)
+    encrypted_secret: Mapped[str] = Column(Text, nullable=False)
+    passphrase_hash: Mapped[str] = Column(String)
+    created_at: Mapped[datetime] = Column(
+        DateTime, default=datetime.now()
+    )
+    expires_at: Mapped[datetime] = Column(DateTime)
+    is_active: Mapped[bool] = Column(Boolean, default=True)
+
+
+class Log(Base):
+    """Secret logs table model"""
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    secret_key: Mapped[str] = Column(String, nullable=False)
+    action: Mapped[str] = Column(String, nullable=False)
+    timestamp: Mapped[datetime] = Column(
+        DateTime, default=datetime.now()
+    )
+    ip_address = Column(String)
